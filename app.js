92039aa9bc10ae9910d7f4d22c273b3fd080aaef
muhammad-tahseen-45 <144385363+muhammad-tahseen-45@users.noreply.github.com>
2023-09-12 07:55:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,303 @@
+    // // cahpter 6 
+
+    // // q1
+
+    // var x = 10;
+    // var y = 20;
+
+    // console.log(x*y);
+    // console.log(x/y);
+
+
+
+    // // q2
+
+    // x = 100;
+    // --x
+    // console.log(x)
+
+
+
+    // 3
+
+    // var x = 50;
+    // var y  = x++;
+    // console.log(y);
+
+
+//  tthe value of z is 50 
+
+    
+
+// 4
+
+
+// var y = 50;
+// var z = --y;
+// console.log(z)
+
+
+// the value of z is 49
+
+
+// 5
+
+
+
+
+
+//  cahpa 7
+
+// 1
+
+ var calculatedNum = 2+(2*6);
+ console.log(calculatedNum);
+
+//   thea value of calculated is 14
+
+// 2
+
+var calculatedNum = (2 + 2) * 6;
+
+console.log(calculatedNum)
+
+//  the value of calculatedNum is 24
+
+
+// 3
+
+var calculatedNum = (2 + 2) * (4 + 2);
+console.log(calculatedNum)
+//  the value of this ques is 24 
+
+
+// 4 
+
+var calculatedNum = ((2 + 2) * 4) + 2;
+console.log(calculatedNum)
+
+//  the value of this ques is 18
+
+
+
+// 5
+
+var costTheResult = ( 2 + 2 )* (4 + 10);
+console.log(costTheResult)
+
+
+
+// 6
+
+ var unittheresult= 2 + 2 * 4 + 10;
+ console.log(unittheresult);
+
+//  7
+
+var pressure =   4 / 2 * 4;
+console.log(pressure);
+
+// chpaater 8 
+
+// 1
+
+var num = "2" + "2";
+console.log(num)
+
+// this question answer is 22
+
+// 2
+message = ("Hello," + "Dolly")
+alert(message);
+
+// this answer is hello,dolly
+
+
+// 3
+
+alert("33" + 3);
+
+// display in the alert box is 333
+
+
+// 4
+
+var country = ("pakistan" + "zindabad")
+alert(country)
+
+
+
+// 5
+
+var name = ("tahseen" + 88 );
+console.log(name);
+
+// 6
+
+var x = "pakistan";
+var y = "babar";
+var  z = ("team "+ "player");
+
+console.log(z);
+
+
+
+
+//  chapter 9
+
+// 1
+
+var username = prompt("enter you first name")
+alert(username)
+
+// 2
+
+var  country = prompt("china")
+alert(country)
+
+// 3
+
+var yourName = prompt("Enter Your Name");
+
+
+// 4
+
+
+
+
+// chapter 10  
+
+// 1
+
+
+var city = "karachi"
+if(city == "karachi"){
+    console.log("the city of light")
+}
+
+
+// 2
+
+var x = prompt()
+
+if(x==="y"){
+    alert()
+}
+
+
+// 3
+
+
+var zipcode = prompt("enter zipcode")
+
+if(zipcode==="10010"){
+    alert("karachi")
+}
+
+else{
+    alert("please write correct city ajayn")
+}
+
+
+// 4
+
+var testVeriable = 3;
+x = 1;
+if(testVeriable === "x"){
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+
+
+
+// chapter 11
+
+
+// 1
+
+var v = "no";
+var s = "yes";
+
+if(v  != s){
+    console.log("hi")
+}
+// 2
+var n = 9;
+var b = 8;
+if(b>=n){
+    console.("hi")
+}
+
+
+
+
+// 3
+
+var v = 3;
+var s = 4;
+
+if(v  != s){
+ alert("hi")
+}
+
+
+
+// 4
+
+var v = 3;
+var s = 4;
+
+if(v  != s){
+ alert("congratulation")
+}
+
+
+// 5
+
+var firstname = prompt("enter your name");
+var  anothername = ""
+
+if(firstname != anothername ){
+ alert("congratulation")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
